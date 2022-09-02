8eddfc90bd93aad6fe68259404bba5fc4a4fdad9
me3870 <me3870@MSS2150-27>
2022-09-02 10:58:21 -0400

    Add project files.

---
+++ BinaryTree.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    internal class BinaryTree<T> where T : IComparable<T>
+    {
+        private Node<T> root;
+
+        public Node<T> Add(T data)
+        {
+            if (root == null)
+            {
+                root = new Node<T>(data);
+                return root;
+            }
+
+            
+
+        }
+        private Node<T> RecursiveAdd(Node<T> node, T data)
+        {
+            if (node == null)
+                throw new NullReferenceException();
+            if (node.Left == null)
+            {
+                Node<T> newNode = new Node<T>(data);
+                return newNode;
+            }
+            if (node.Right == null)
+            {
+                Node<T> newNode = new Node<T>(data);
+                node.Right = newNode;
+                return newNode;
+            }
+        }
+    }
+}
---
+++ Node.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    internal class Node<T> where T : IComparable<T>
+    {
+        private readonly T? data;
+        private readonly Node<T>? left, right;
+
+        public Node(T data) { 
+            this.data = data;
+            left = right = null;
+        }
+        public Node(T data, Node<T> left) : this(data)
+        {
+            this.left = left;
+        }
+        public Node(T data, Node<T> left, Node<T> right) : this(data, left)
+        {
+            this.right = right;
+        }
+
+        public T Data { get { return data; } } 
+        public Node<T> Left { get { return left; } }
+        public Node<T> Right { get { return right; } }
+    }
+}
